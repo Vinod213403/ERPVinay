57ca87bd22ad3ba8c3c8881138a538c29cf7f638
Rithi <Rithi@RITHIKESH>
2024-06-22 15:00:08 +0530

    New class added

---
+++ Hybrid_FrameWork/src/test/java/commonFunctions/Demotesting.java
@@ -0,0 +1,26 @@
+package commonFunctions;
+
+public class Demotesting {
+	public void add()
+	{
+		int a=10;
+		int b=35;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public void sub()
+	{
+		int a=100;
+		int b=35;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public void div()
+	{
+		int a=100;
+		int b=25;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
